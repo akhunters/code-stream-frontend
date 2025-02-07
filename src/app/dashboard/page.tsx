d05b7c1b7475e4b7d4fe418309bc018f21b208a6
akhunters <avinashkmr886@gmail.com>
2025-02-07 20:58:21 +0530

    chore(refactor): refactor dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import { getAllPosts } from "@/actions/posts.action";
 import { auth } from "@/auth";
 import { BlogPost } from "@/components/molecules/blog-post";
-import { Separator } from "@/components/ui/separator";
 import { redirect } from "next/navigation";
 
 const DashboardPage = async () => {
@@ -15,16 +14,14 @@ const DashboardPage = async () => {
         authorId: session.user.userId,
     });
 
-    const updatedPosts = new Array(5).fill(posts[0]);
 
     return (
         <div className="flex flex-col min-h-screen px-12 py-20 gap-x-8">
             <h3 className="text-4xl font-black text-gray-800 text-center">My Blogs</h3>
             <div className="w-full grid grid-cols-4 gap-6">
-                {updatedPosts.map((post, idx) => (
+                {posts.map((post) => (
                     <div key={post.id} >
                         <BlogPost key={post.id} {...post} thumbnail="/images/dummy-thumbnail.png" editable />
-                        {idx !== posts.length - 1 && <Separator />}
                     </div>
                 ))}
             </div>
