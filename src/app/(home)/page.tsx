0aab1ef53ff5b90915d9190ed13c2e80c20bb29a
akhunters <avinashkmr886@gmail.com>
2025-02-08 17:21:31 +0530

    feat(home): fix blog post styling remove separator

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
 import { getAllPosts } from "@/actions/posts.action";
 import { BlogPost } from "@/components/molecules/blog-post";
-import { Separator } from "@/components/ui/separator";
 
 export default async function Home() {
   const posts = await getAllPosts();
@@ -10,15 +9,12 @@ export default async function Home() {
   return (
     <div className="flex flex-col min-h-screen px-12 py-20 gap-x-8">
       <div className="w-full grid grid-cols-4 gap-6">
-        {posts.map((post, idx) => (
-          <div key={post.id}>
-            <BlogPost
-              key={post.id}
-              {...post}
-              thumbnail="/images/dummy-thumbnail.png"
-            />
-            {idx !== posts.length - 1 && <Separator />}
-          </div>
+        {posts.map((post) => (
+          <BlogPost
+            key={post.id}
+            {...post}
+            thumbnail="/images/dummy-thumbnail.png"
+          />
         ))}
       </div>
     </div>
