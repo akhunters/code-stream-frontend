87b6440f61990e6d36ef926c9b3929872015f0e3
akhunters <avinashkmr886@gmail.com>
2025-02-08 17:26:32 +0530

    feat(error-handling): add error pages to all routes

---
+++ src/components/templates/error-template.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Icon } from "@/components/atoms/icon";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export interface ErrorPageProps { error: Error; reset: () => void }
+
+export function ErrorPageTemplate({ error, reset }: ErrorPageProps) {
+    return (
+        <div className="flex flex-col items-center justify-center h-[70vh] text-center space-y-6 min-h-screen">
+            <div className="w-64 h-64 flex items-center justify-center rounded-lg">
+                <span className="text-8xl">💥🐛</span>
+            </div>
+            <h2 className="text-3xl font-semibold text-gray-700">
+                Uh-oh! Looks like an <span className="text-red-600">&quot;exception&quot;</span> was thrown! 🚨
+            </h2>
+
+            <p className="text-gray-500 max-w-lg">
+                Something went wrong, and your request couldn&apos;t be completed.
+                Maybe there’s a missing **try/catch block** in your code? 🤔
+                Don’t worry, you can debug and **return to safety**.
+            </p>
+
+            <div className="p-4 bg-gray-100 border border-gray-300 rounded-lg max-w-lg text-sm text-gray-700">
+                <strong>Error Message:</strong> {error.message}
+            </div>
+            <div className="flex gap-4">
+                <Button
+                    className="flex gap-2 px-6 py-6 bg-orange-600 text-white text-md rounded-lg shadow-md hover:bg-orange-700 transition"
+                    onClick={reset}
+                >
+                    <Icon name="refresh-cw" strokeWidth={2} />
+                    <span>Retry</span>
+                </Button>
+                <Link href="/">
+                    <Button className="flex gap-2 px-6 py-6 bg-blue-600 text-white text-md rounded-lg shadow-md hover:bg-blue-700 transition">
+                        <Icon name="home" strokeWidth={2} />
+                        <span>Return to Homepage</span>
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
+}
