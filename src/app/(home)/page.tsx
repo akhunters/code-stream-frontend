d8b7ef7d0a2eba8083bdbbc907b9bedd13c9a7ba
akhunters <avinashkmr886@gmail.com>
2025-02-08 17:29:55 +0530

    refactor(testing-code): remove code to test out lazy loading

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,8 +4,6 @@ import { BlogPost } from "@/components/molecules/blog-post";
 export default async function Home() {
   const posts = await getAllPosts();
 
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
   return (
     <div className="flex flex-col min-h-screen px-12 py-20 gap-x-8">
       <div className="w-full grid grid-cols-4 gap-6">
