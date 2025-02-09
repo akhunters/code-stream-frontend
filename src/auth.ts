670f541b069c29db0e801656de7bf71ac54a4490
akhunters <avinashkmr886@gmail.com>
2025-02-09 17:47:48 +0530

    refactor(auth.ts): add jsdoc and moved callbacks in separate functions

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,61 @@
-import NextAuth, { User } from "next-auth";
+import NextAuth, { Account, Session, User } from "next-auth";
 import 'next-auth/jwt';
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import { backendLogin } from "./actions/auth-backend.action";
 import { AuthProvider } from "./types/auth.type";
 import { AdapterUser } from "next-auth/adapters";
+import { JWT } from "next-auth/jwt";
 
 const MAX_AGE = 60 * 60;
+
+/**
+  * This function will be called to authenticate each request
+  * being made to the Next.js server
+  * @param token The JWT token
+  * @param account The user's account information
+  * @returns The JWT token with the user's information
+*/
+const jwtCallback = async ({ token, account }: {
+    token: JWT,
+    account: Account | null
+}): Promise<JWT> => {
+    if (account?.access_token && account?.provider) {
+        /**
+            * Call the backend to authenticate the user with the provider's access token
+            * This code block will only be executed when the user signs in
+        */
+        const backendAuthenticateResponse = await backendLogin({
+            token: account.access_token,
+            provider: account.provider as AuthProvider,
+        });
+
+        // Store the authenticated user's information in the token
+        token.accessToken = backendAuthenticateResponse.accessToken;
+        token.accessTokenExpires = backendAuthenticateResponse.expiresAt;
+        token.userId = backendAuthenticateResponse.user.id;
+    }
+
+    return token;
+}
+
+/**
+ * This function will be called to create the user's session
+ * @param session The user's session
+ * @param token The JWT token
+ * @returns The user's session
+*/
+const sessionCallback = async ({ session, token }: {
+    session: Session,
+    token: JWT
+}) => {
+    // Store the user's information in the session
+    // to easily access it throughout the application
+    session.accessToken = token.accessToken;
+    session.accessTokenExpires = token.accessTokenExpires;
+    session.user.userId = token.userId;
+    return session;
+}
 
 const {
     auth,
@@ -16,14 +65,8 @@ const {
 } = NextAuth({
     trustHost: true,
     providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-        FacebookProvider({
-            clientId: process.env.FACEBOOK_CLIENT_ID!,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
-        }),
+        GoogleProvider,
+        FacebookProvider,
     ],
     jwt: {
         maxAge: MAX_AGE,
@@ -32,26 +75,8 @@ const {
         maxAge: MAX_AGE,
     },
     callbacks: {
-        async jwt({ token, account }) {
-            if (account?.access_token && account?.provider) {
-                const backendAuthenticateResponse = await backendLogin({
-                    token: account.access_token,
-                    provider: account.provider as AuthProvider,
-                });
-
-                token.accessToken = backendAuthenticateResponse.accessToken;
-                token.accessTokenExpires = backendAuthenticateResponse.expiresAt;
-                token.userId = backendAuthenticateResponse.user.id;
-            }
-
-            return token;
-        },
-        async session({ session, token }) {
-            session.accessToken = token.accessToken;
-            session.accessTokenExpires = token.accessTokenExpires;
-            session.user.userId = token.userId;
-            return session;
-        },
+        jwt: jwtCallback,
+        session: sessionCallback,
     },
     pages: {
         signIn: "/signin",
@@ -60,6 +85,9 @@ const {
 
 export { auth, handlers, signIn, signOut };
 
+/**
+ * Extend the NextAuth session to include the user's information
+ */
 declare module "next-auth" {
     interface Session {
         accessToken: string;
@@ -68,7 +96,9 @@ declare module "next-auth" {
     }
 }
 
-
+/**
+ * Extend the NextAuth JWT to include the user's information
+ */
 declare module 'next-auth/jwt' {
     interface JWT {
         accessToken: string;
